12ba733f5e55f8dd98b492ecfcb06f361843fb48
Douglas R. Miles <logicmoo@gmail.com>
2015-06-20 03:01:42 -0700

    Update test_func_11.pl

--- t/test_func_11.pl
+++ t/test_func_11.pl
@@ -6,7 +6,7 @@
 :- module(test_func_11, [main/0]).
 
 :- use_module(library(func)).
-:- use_module(library(compose)).
+:- use_module(test_func_11_compose).
  
 main :-
   Compose = number_string of add_one,
